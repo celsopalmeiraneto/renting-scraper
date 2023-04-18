0167e12a774d8954fdf89c4871ff751b3caac0b8
Celso Palmeira Neto <neto@celsoneto.com.br>
2023-04-18 14:02:39 +0100

    feat: enable headless by default

--- src/scrapers/ScraperReadable.ts
+++ src/scrapers/ScraperReadable.ts
@@ -24,15 +24,10 @@ export abstract class ScraperReadable extends Readable {
   async _construct(callback: (error?: Error | null | undefined) => void): Promise<void> {
     try {
       this.browser = await firefox.launch({
-        headless: false,
+        headless: true,
       });
       this.context = await this.browser.newContext();
-      this.page = await this.context.newPage({
-        screen: {
-          height: 768,
-          width: 1366,
-        },
-      });
+      this.page = await this.context.newPage();
       await this._initialize();
 
       return callback();
